9fc40329635187eb42b33f1e6aa0d4bdb49579fd
Debasish Pandey <debasishpandey9178@gmail.com>
2025-04-09 15:39:38 +0530

    db uploaded to cloud

--- src/main/java/com/util/DbConnection.java
+++ src/main/java/com/util/DbConnection.java
@@ -5,10 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
+   private static final String jdbcUrll = "jdbc:mysql://interchange.proxy.rlwy.net:41074/railway?user=root&password=PUltKYqJmyBoklImskytMnQQjHyaFCMA";
+
     public  Connection getConnection() {
         Connection con = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(jdbcUrll);
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
